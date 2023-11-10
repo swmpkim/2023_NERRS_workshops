af1506fed36bac35bd2e642dbda23de857f0921c
Kim Cressman <kim@catbirdstats.com>
2023-11-10 09:32:08 -0600

    added example code files for intro course

---
+++ 1 - Intro/example_files/02_example.R
@@ -0,0 +1,39 @@
+# Intro ----
+# This is a regular .R script  
+# Lines that start with a # are seen as comments rather than
+# as code to run  
+
+# You can run a single line of code 
+# with the keyboard shortcut `Ctrl`/`Cmd` + `Enter` 
+# or by selecting "Run Selected Line(s)" from the "Run" button 
+# in the upper right side of this pane. 
+
+# You can run the entire script with the "Source" button
+# in the upper right side of this pane.  
+
+# Lines that have four dashes at the end 
+# make a "Header" that you can see in the document outlines
+# stacked lines button, top right of pane; and
+# at the bottom left of the pane  
+
+
+# Executable code ----
+
+# Here are some lines of code to run.  
+
+1 + 1
+
+x <- 2  
+y <- 1:5
+
+x + y
+
+# Working directory ----
+
+getwd()
+
+# paste the path that results from the above command as a comment below:
+# 
+
+# Your turn ----
+# write some code and run it  
